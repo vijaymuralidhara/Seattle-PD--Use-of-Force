fa23d02fd9ed94b6b450126475274312017c6e41
vijaymuralidhara <vijaym@uw.edu>
2019-12-03 17:21:57 -0800

    Completion of P3

--- Visualizations.R
+++ Visualizations.R
@@ -22,13 +22,20 @@ race_force <- force_data %>%
 
 bar <- ggplot(data = race_force, aes(x = Officer.Race, y = n,
                                      fill = Use.of.Force.Indicator,
-                                     label = n)) +
-  geom_bar(stat = "identity") +
-  geom_text(size = 3, position = position_stack(vjust = 0.5)) +
-  labs(y = "Number of Instances", x = "Race of Officer") +
-  scale_fill_discrete(name = "Force Used: (Y)es or (N)o") +
-  scale_y_continuous(limits = c(0, 4000)) +
-  ggtitle("Use of Force and Officer Race Correlation")
+              label = n)) +
+  geom_bar(position = position_dodge(), 
+           stat = "identity",
+           width = 0.6,
+           color = "black",
+           size = 0.5,
+           alpha = 0.7) +
+  
+   geom_text(size = 3, position = position_dodge(width = 0.7), vjust = -0.5, check_overlap = TRUE) +
+   labs(y = "Number of Instances", x = "Race of Officer") +
+   scale_fill_discrete(name = "Force Used: (Y)es or (N)o") +
+  # scale_y_continuous(limits = c(0, 4000)) +
+   ggtitle("Use of Force and Officer Race Correlation")
+
 
 # Use of Force vs Officer Per Precinct
 precinct_force <- force_data %>%
@@ -42,8 +49,13 @@ precinct_force <- force_data %>%
 bar2 <- ggplot(data = precinct_force, aes(x = Precinct, y = n,
                                           fill = Use.of.Force.Indicator,
                                           label = n)) +
-  geom_bar(stat = "identity") +
-  geom_text(size = 3, position = position_stack(vjust = 0.5)) +
+  geom_bar(position = position_dodge(), 
+           stat = "identity",
+           width = 0.6,
+           color = "black",
+           size = 0.5,
+           alpha = 0.7) +
+  geom_text(size = 3, position = position_dodge(width = 0.7), vjust = -0.5, check_overlap = TRUE) +
   labs(y = "Number of Instances", x = "Precinct") +
   scale_fill_discrete(name = "Force Used: (Y)es or (N)o") +
   scale_y_continuous(limits = c(0, 17000)) +
@@ -58,8 +70,13 @@ gender_force <- force_data %>%
 bar3 <- ggplot(data = gender_force, aes(x = Officer.Gender, y = n,
                                         fill = Use.of.Force.Indicator,
                                         label = n)) +
-  geom_bar(stat = "identity") +
-  geom_text(size = 3, position = position_stack(vjust = 0.5)) +
+  geom_bar(position = position_dodge(), 
+           stat = "identity",
+           width = 0.6,
+           color = "black",
+           size = 0.5,
+           alpha = 0.7) +
+  geom_text(size = 3, position = position_dodge(width = 0.7), vjust = -0.5, check_overlap = TRUE) +
   labs(y = "Number of Instances", x = "Gender of Officer") +
   scale_fill_discrete(name = "Force Used: (Y)es or (N)o") +
   scale_y_continuous(limits = c(0, 55000)) +
@@ -72,11 +89,11 @@ exp_force <- force_data %>%
   count(Use.of.Force.Indicator)
 
 plot <- ggplot(data = exp_force, aes(x = Officer.Years.of.Experience, y = n,
-                                     label = n, group = 1)) +
-  geom_point(aes(shape = Use.of.Force.Indicator)) +
-  geom_smooth() +
+                                     label = n, color = Use.of.Force.Indicator)) +
+  geom_smooth(se = FALSE) +
+  geom_point(aes(size = 1), show.legend = FALSE) +
   labs(y = "Number of Instances", x = "Officer Years of Experience",
-       shape = "Force Used: (Y)es or (N)o") +
+       color = "Force Used: (Y)es or (N)o") +
   #scale_shape_discrete(name = "Force Used: (Y)es or (N)o") +
   ggtitle("Use of Force and Officer Experience Correlation")
 
@@ -95,6 +112,5 @@ time_force <- force_data %>%
 plot2 <- ggplot(data = time_force, aes(x = times, y = n, group = 1)) +
   geom_line(color = "navyblue", size = 1.5) +
   labs(y = "Number of Instances Force was Used", x = "Reported Time") +
-  #scale_fill_discrete(name = "Force Used: (Y)es or (N)o") +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   ggtitle("Use of Force and Reported Time Correlation")
